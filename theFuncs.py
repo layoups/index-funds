2248efb725257434f36caec1cd5e9a928b69516c
layoups <layounkarim@gmail.com>
2021-11-28 23:02:07 +0100

    first index

--- theFuncs.py
+++ theFuncs.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from statsmodels.regression.rolling import RollingOLS
+from datetime import datetime
 
 import quandl
 import pandas_datareader.data as web
